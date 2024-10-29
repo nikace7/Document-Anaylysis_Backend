30e0d016c5336e26f62e84283bb2ca101d0c95a2
nikace7 <khatrinikace@gmail.com>
2024-10-29 09:21:23 +0545

    feat add admin page for models

--- backend/file_to_table/admin.py
+++ backend/file_to_table/admin.py
@@ -1,3 +1,90 @@
 from django.contrib import admin
+from .models import (
+    User,
+    VerificationEmail,
+    FileInput,
+    FilePage,
+    ImageInput,
+    WordConversion,
+    ScannedFile,
+    ScannedImage,
+    GuestScannedFile,
+    GuestScannedImage,
+    PasswordResetRequest,
+    BugReport,
+    BugImage,
+    ContactForm,
+)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'contact', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'contact')
+    list_filter = ('is_staff', 'is_active')
+
+@admin.register(VerificationEmail)
+class VerificationEmailAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_on', 'status')
+    search_fields = ('user__email',)
+    list_filter = ('status',)
+
+@admin.register(FileInput)
+class FileInputAdmin(admin.ModelAdmin):
+    list_display = ('user', 'file', 'created')
+    search_fields = ('user__email',)
+    list_filter = ('created',)
+
+@admin.register(FilePage)
+class FilePageAdmin(admin.ModelAdmin):
+    list_display = ('file', 'created')
+    search_fields = ('file__file',)
+
+@admin.register(ImageInput)
+class ImageInputAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image', 'created')
+    search_fields = ('user__email',)
+
+@admin.register(WordConversion)
+class WordConversionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created', 'image', 'document')
+    search_fields = ('user__email',)
+
+@admin.register(ScannedFile)
+class ScannedFileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'file', 'created')
+    search_fields = ('user__email',)
+
+@admin.register(ScannedImage)
+class ScannedImageAdmin(admin.ModelAdmin):
+    list_display = ('file', 'created')
+    search_fields = ('file__file',)
+
+@admin.register(GuestScannedFile)
+class GuestScannedFileAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'file', 'created')
+    search_fields = ('identifier',)
+
+@admin.register(GuestScannedImage)
+class GuestScannedImageAdmin(admin.ModelAdmin):
+    list_display = ('file', 'created')
+    search_fields = ('file__file',)
+
+@admin.register(PasswordResetRequest)
+class PasswordResetRequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created', 'is_active', 'code')
+    search_fields = ('user__email',)
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('user', 'description')
+    search_fields = ('user__email', 'description')
+
+@admin.register(BugImage)
+class BugImageAdmin(admin.ModelAdmin):
+    list_display = ('report', 'image')
+    search_fields = ('report__description',)
+
+@admin.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'description')
+    search_fields = ('name', 'email', 'subject')
